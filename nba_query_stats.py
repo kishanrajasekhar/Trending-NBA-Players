12d6ec82e3a376e8a58a2abd79797f9384b76243
Kishan Rajasekhar <kishan.rajasekhar@wdc.com>
2020-06-14 17:35:34 -0600

    Running the nba_query_stats script will give you cumulative fantasy points of each player between the specified start date and end date

--- nba_query_stats.py
+++ nba_query_stats.py
@@ -1,5 +1,7 @@
 import argparse
 from datetime import date
+from database_objects.player_stats import PlayerStats
+from mongoengine import connect
 
 
 def setup_query_parser() -> argparse.ArgumentParser:
@@ -19,6 +21,60 @@ def setup_query_parser() -> argparse.ArgumentParser:
     return parser
 
 
+def query_nba_stats(start_date, end_date) -> []:
+    """Query the nba stats of players from all the games starting from the specified start date to the end date
+    (inclusive).
+
+    :param start_date: start date of when to start accumulating the stats (type: datetime.datetime)
+    :param end_date: end date of when to stop accumulating the stats (type: datetime.datetime)
+    :return: list of dicts (format: [ {"name": "Nikola Jokić", "fantasy_points": 1784.5}, ... ])
+    """
+    player_stats = PlayerStats.objects(game_date__gte=start_date, game_date__lte=end_date)
+
+    pipeline = [
+        {
+            '$group': {
+                '_id': '$player',
+                'fantasy_points': {
+                    '$sum': '$fantasy_points'
+                }
+            }
+        }, {
+            '$sort': {
+                'fantasy_points': -1
+            }
+        }, {
+            '$lookup': {
+                'from': 'player',
+                'localField': '_id',
+                'foreignField': '_id',
+                'as': 'player'
+            }
+        }, {
+            '$project': {
+                '_id': 0,
+                'fantasy_points': 1,
+                'name': {
+                    '$concat': [
+                        {
+                            '$arrayElemAt': [
+                                '$player.first_name', 0
+                            ]
+                        }, ' ', {
+                            '$arrayElemAt': [
+                                '$player.last_name', 0
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    ]
+
+    results = list(player_stats.aggregate(*pipeline))
+    return results
+
+
 if __name__ == '__main__':
     args = setup_query_parser().parse_args()
 
@@ -33,10 +89,16 @@ if __name__ == '__main__':
         start_date = date(year=year_start, month=month_start, day=day_start)
         end_date = date(year_end, month=month_end, day=day_end)
 
-        if start_date < end_date:
-            print("Query stats!")
+        if start_date <= end_date:
+            connect('nba')
+            print()
+            print(f"Collecting cumulative fantasy points for start date: {start_date} and end date: {end_date}")
+            print()
+            nba_data = query_nba_stats(start_date, end_date)
+            for data in nba_data:
+                print("Player:", data["name"]+",", "Fantasy Points:", data["fantasy_points"])
         else:
-            print("Start date must be before end date.")
+            print("End date cannot be after start date.")
             print(f"You specified start date: {start_date} and end date: {end_date}")
 
     except ValueError:
